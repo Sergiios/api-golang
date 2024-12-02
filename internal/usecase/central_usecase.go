36a6c3e453ffba5aae701d21b38243c6e0349cff
Sergio Santos <44881707+Sergiios@users.noreply.github.com>
2024-12-02 03:42:20 +0000

    feat: add unit tests for usecases

--- internal/usecase/central_usecase.go
+++ internal/usecase/central_usecase.go
@@ -2,19 +2,26 @@ package usecase
 
 import (
 	"api-golang/internal/domain"
-	"api-golang/internal/repository"
 )
 
-type CentralUseCase struct {
-	Repo *repository.CentralRepository
+type CentralRepository interface {
+	Create(user *domain.Central) error
+	GetAll() ([]domain.Central, error)
+	GetByID(id uint) (*domain.Central, error)
+	Update(user *domain.Central) error
+	Delete(id uint) error
 }
 
-func NewCentralUseCase(repo *repository.CentralRepository) *CentralUseCase {
+type CentralUseCase struct {
+	Repo CentralRepository
+}
+
+func NewCentralUseCase(repo CentralRepository) *CentralUseCase {
 	return &CentralUseCase{Repo: repo}
 }
 
-func (uc *CentralUseCase) CreateCentral(user *domain.Central) error {
-	return uc.Repo.Create(user)
+func (uc *CentralUseCase) CreateCentral(central *domain.Central) error {
+	return uc.Repo.Create(central)
 }
 
 func (uc *CentralUseCase) GetAllCentrals() ([]domain.Central, error) {
@@ -25,8 +32,8 @@ func (uc *CentralUseCase) GetCentralByID(id uint) (*domain.Central, error) {
 	return uc.Repo.GetByID(id)
 }
 
-func (uc *CentralUseCase) UpdateCentral(user *domain.Central) error {
-	return uc.Repo.Update(user)
+func (uc *CentralUseCase) UpdateCentral(central *domain.Central) error {
+	return uc.Repo.Update(central)
 }
 
 func (uc *CentralUseCase) DeleteCentral(id uint) error {
